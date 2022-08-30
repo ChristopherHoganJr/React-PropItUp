4c66b55c2f6858d185696cfa02923743a14fe3ba
chris hogan <christopherhoganjr@icloud.com>
2022-08-30 15:31:08 -0700

    modified for assignment Putting it Together

--- propitup/src/components/PersonCard.js
+++ propitup/src/components/PersonCard.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
 export class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: parseInt(this.props.age),
+    };
+  }
+
+  updateAge = (e) => {
+    this.setState({ age: (this.state.age += 1) });
+  };
+
   render() {
-    const { firstName, lastName, age, hairColor } = this.props;
     return (
       <div>
         <h2>
-          {lastName}, {firstName}
+          {this.props.lastName}, {this.props.firstName}
         </h2>
-        <p>Age: {age}</p>
-        <p>Hair Color: {hairColor}</p>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+        <button onClick={this.updateAge}>Add year to age</button>
       </div>
     );
   }
